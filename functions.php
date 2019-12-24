6c2b6f674f027f54e841fcd1c3e432671f48ef00
Hironobu Kimura <kimura@emotionale.jp>
2019-12-24 13:25:20 +0900

    update version

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@
  * Theme Name: kikoiro1
  * Author: Hironobu Kimura
  * Author URI: https://www.emotionale.jp/
- * Version: 1.02
+ * Version: 1.03
  * kikoiro1 is based on Underscores twentynineteen by WordPress team.
  * twentynineteen is distributed under the terms of the GNU GPL v2 or later.
  * License: No License
