9c7897eebbf5ea65e0a172a993cc9044cbcc0461
Hironobu Kimura <kimura@emotionale.jp>
2019-12-30 12:49:56 +0900

    transform update

--- gutenberg/build/index.asset.php
+++ gutenberg/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-editor', 'wp-element', 'wp-polyfill', 'wp-rich-text'), 'version' => '590de5359aec8fd6cbe3e60ea5b1cde2');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-editor', 'wp-element', 'wp-polyfill', 'wp-rich-text'), 'version' => 'e1b778726f180280261e56498b371104');
