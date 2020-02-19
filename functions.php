d8a8e20bd636178c6a0079ad2ecac5e16346c5f1
Hironobu Kimura <kimura@emotionale.jp>
2020-02-19 23:28:06 +0900

    fix ranking thumbnail quality

--- functions.php
+++ functions.php
@@ -223,6 +223,12 @@ function excerptByLength($length) {
 	return $content;
 }
 
+function jetpack_custom_thumb_size( $get_image_options ) {
+	$get_image_options['avatar_size'] = 96;
+	return $get_image_options;
+}
+add_filter( 'jetpack_top_posts_widget_image_options', 'jetpack_custom_thumb_size' );
+
 function isNewsAndColumnCagetory() {
 	$currentCategory = get_category(the_category_ID(false), false);
 	if ($currentCategory->parent == 0) { return false; }
